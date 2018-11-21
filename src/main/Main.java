ac83d0af29a4d21b3562d60d4caa8324ab0ac46d
Wildrimak de Souza Pereira <wildrimak@infoway-pi.com.br>
2018-11-21 08:05:42 -0300

    Teste 0

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,11 +5,31 @@ import java.util.List;
 
 import models.Box;
 
+/*
+ * Objetivo: Facilitar a leitura de codigos com generics
+ * 
+ * Metas:
+ * 
+ * 1) Criar uma classe com um generic e testar a criação de objetos dessa classe
+ * 
+ * 2) Criar uma classe com atributos que tenham generics e ver o funcionamento deles com exemplos publicos e privados (gets e sets)
+ * 
+ * 3) Criar uma classe com metodos que tenham parametros genericos simples
+ * 
+ * 4) Criar uma classe com metodos cujo retorno seja generic (ver a diferenca e os impactos entre "public <T> getT();" e "public T getT();"
+ * 
+ * 5) Criar uma classe com metodos cujo parametros contenham (? super T)
+ * 
+ * 6) Criar uma classe com generics que contenha generics (? super T)
+ * 
+ * 7) Criar uma classe com generics que contenha generics (? super T) que é filha de uma classe sem generics e com generics
+ * */
+
 public class Main {
 
 	public static void main(String[] args) {
 		
-		Box box0 = new Box();
+		Box<Object> box0 = new Box<Object>();
 		box0.setT(new Object());
 		System.out.println(box0.getT());
 		
@@ -19,7 +39,9 @@ public class Main {
 		Box<String> box2 = new Box<String>("Kamirdliw");
 		System.out.println(box2.getT());
 		
-		List<Box> boxes_genericas;
+		List<Box<Object>> boxes_genericas = new ArrayList<>();
+		
+		boxes_genericas.add(new Box<Object>());
 		
 		List<Box<String>> boxes_strings = new ArrayList<>();
 		
